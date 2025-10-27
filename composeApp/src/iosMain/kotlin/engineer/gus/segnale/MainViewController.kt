2d82ffc331a5f0ced4f37df25064c34da13c20f9
Gus Narea <gnarea@users.noreply.github.com>
2025-10-27 00:06:53 +0000

    fix detekt issues

--- composeApp/src/iosMain/kotlin/engineer/gus/segnale/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/engineer/gus/segnale/MainViewController.kt
@@ -2,4 +2,4 @@ package engineer.gus.segnale
 
 import androidx.compose.ui.window.ComposeUIViewController
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun mainViewController() = ComposeUIViewController { App() }
